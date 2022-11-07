91f4a1573b1ea915b626f9a34528a135f7f3785a
brian <brian@COMPUTER>
2022-11-06 18:20:29 -0800

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVCWebApp.Models;
+
+namespace MVCWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MVCWebApp.Models.Joke> Joke { get; set; }
+    }
+}
